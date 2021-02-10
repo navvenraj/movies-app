89141eba14d35691731513dbad4f512f37231eb4
Navven Raj MN <navvenr96@gmail.com>
2021-02-10 23:07:26 +0530

    Added ‘Home’ component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './screens/home/Home';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<Home/>,
   document.getElementById('root')
 );
 
